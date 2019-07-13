1597f50dbb4aba0485359928f574baf4c4a10752
wangzhiqiang <1071683940@qq.com>
2019-07-13 12:31:01 +0800

    chore:  添加页面适配
    # head: <type>: <subject>
    # - type:
    # - feat：添加新功能。
    # - fix：修复 bug。
    # - style：代码格式。
    # - css：样式相关。
    # - pref：优化相关。
    # - revert：分支回溯。
    # - chore：构建配置相关。
    # - docs：文档相关。
    # - build: 打包相关
    
    # - subject: start with verb (such as 'change'), 50-character line

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -82,26 +82,7 @@ module.exports = function(webpackEnv) {
         // Options for PostCSS as we reference these options twice
         // Adds vendor prefixing based on your specified browser support in
         // package.json
-        loader: require.resolve('postcss-loader'),
-        options: {
-          // Necessary for external CSS imports to work
-          // https://github.com/facebook/create-react-app/issues/2677
-          ident: 'postcss',
-          plugins: () => [
-            require('postcss-flexbugs-fixes'),
-            require('postcss-preset-env')({
-              autoprefixer: {
-                flexbox: 'no-2009'
-              },
-              stage: 3
-            }),
-            // Adds PostCSS Normalize as the reset css with default options,
-            // so that it honors browserslist config in package.json
-            // which in turn let's users customize the target behavior as per their needs.
-            postcssNormalize()
-          ],
-          sourceMap: isEnvProduction && shouldUseSourceMap
-        }
+        loader: require.resolve('postcss-loader')
       }
     ].filter(Boolean);
     if (preProcessor) {
